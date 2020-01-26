c902022a99efc47b27866d6320e0ac73fe65479c
carpanla <carpanla@gmail.com>
2020-01-26 19:17:30 +0100

    add title and font family

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import '../stylesheets/App.css';
+import Title from './Title';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+  // constructor(props) {
+  //   super(props);
+  // }
 
   render() {
     return (
-      <div className="App">
-       holi
-      </div>
+      <Title />
     );
   }
 }
---
+++ src/components/Title.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../stylesheets/title.css';
+
+function Title(){
+    return(
+        <div className="title__container">
+            <h1 className="title__name">I'm Laura Carril.<br /> I'm a frontend developer.</h1>
+        </div>
+    )
+}
+
+
+
+
+export default Title;
+
